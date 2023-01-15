7b869b148fc9aa5ab96a5c81cf41690ac532c497
Armando Ocampo <fooseaov@gmail.com>
2023-01-15 17:34:52 -0600

    script de trabajo

--- scripts/script_de_trabajo.R
+++ scripts/script_de_trabajo.R
@@ -5,9 +5,10 @@ meta <- read_csv("data/META.csv")
 ggplot(meta, aes(Date, Close)) +
   geom_line(color = 'dodgerblue', size = 2) +
   geom_point(size = 2) +
-  theme_classic() +
-  xlab('Fecha') +
-  ylab('Precio por acción [USD]') +
-  scale_x_date(date_labels="%d %b",date_breaks  ="1 day")
+  theme_bw() +
+  xlab('Marzo-Abril 2018') +
+  ylab('Precio por acción META [USD]') +
+  scale_x_date(date_labels="%d %b",date_breaks  ="1 day") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 0.5, vjust = 0.7))
 
 
