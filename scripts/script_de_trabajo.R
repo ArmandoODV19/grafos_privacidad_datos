220bba50fa31b5cb8430e4014aa20301cf42e34c
Armando Ocampo <fooseaov@gmail.com>
2023-01-15 17:42:23 -0600

    creando dataframe de pago

--- scripts/script_de_trabajo.R
+++ scripts/script_de_trabajo.R
@@ -15,3 +15,14 @@ ggplot(meta, aes(Date, Close)) +
   theme(axis.text.x = element_text(angle = 45, hjust = 0.5, vjust = 0.7))
 
 
+# pago Trump a CA
+
+fecha <- c("2016-07-29", "2016-08-18", "2019-09-01", "2016-10-19", "2016-12-12")
+
+fecha <- as.Date(fecha)
+
+pago <- c(100000, 250000, 5000000, 250000, 312000)
+
+# creando data frame
+df <- data.frame(fecha, pago)
+print(df)
