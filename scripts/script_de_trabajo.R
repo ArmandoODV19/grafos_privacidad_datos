f51c4b73b543249d05ac01373d0a634ee282fe9b
Armando Ocampo <fooseaov@gmail.com>
2023-01-15 17:27:23 -0600

    acciones meta

--- scripts/script_de_trabajo.R
+++ scripts/script_de_trabajo.R
@@ -1,1 +1,13 @@
 # script de trabajo
+
+meta <- read_csv("data/META.csv")
+
+ggplot(meta, aes(Date, Close)) +
+  geom_line(color = 'dodgerblue', size = 2) +
+  geom_point(size = 2) +
+  theme_classic() +
+  xlab('Fecha') +
+  ylab('Precio por acción [USD]') +
+  scale_x_date(date_labels="%d %b",date_breaks  ="1 day")
+
+
